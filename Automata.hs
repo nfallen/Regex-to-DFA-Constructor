e23dcfd303b989dfd79449530e4eb18d9dbf6228
Nova Fallen <nfallen@seas.upenn.edu>
2017-04-15 14:29:51 -0400

    test/refactor for unionThompson

--- Automata.hs
+++ Automata.hs
@@ -28,11 +28,12 @@ type Dtransition = Map (State, Char) State
 -- and one state/symbol pair can map to multiple next states.
 type Ntransition = Map (State, Maybe Char) (Set State)
 
-type States = Map State Bool
+type States = Set State
 
 data NFA = NFA {
                  nstart :: State,
                  nstates :: States,
+                 naccept :: States,
                  ntransition :: Ntransition,
                  nalphabet :: Set Char
                } deriving (Show)
@@ -40,24 +41,36 @@ data NFA = NFA {
 data DFA = DFA {
                  dstart :: State, 
                  dstates :: States, 
+                 daccept :: States,
                  dtransition :: Dtransition, 
                  dalphabet :: Set Char
                } deriving (Show)
 
 -- DFA that accepts all strings
 sigmaStarDfa :: Set Char -> DFA
-sigmaStarDfa ab = DFA {dstart = 0, dstates = Map.singleton 0 True, dtransition = Map.empty, dalphabet = ab}
+sigmaStarDfa ab = DFA {dstart = 0, 
+                       dstates = Set.singleton 0,
+                       daccept = Set.singleton 0,
+                       dtransition = Map.empty,
+                       dalphabet = ab}
 
 -- DFA that rejects all strings
 emptySetDfa :: Set Char -> DFA
-emptySetDfa ab = DFA {dstart = 0, dstates = Map.singleton 0 False, dtransition = Map.empty, dalphabet = ab}
+emptySetDfa ab = DFA {dstart = 0,
+                      dstates = Set.singleton 0,
+                      daccept = Set.empty,
+                      dtransition = Map.empty,
+                      dalphabet = ab}
 
 -- DFA that accepts only the empty string
 emptyStringDfa :: Set Char -> DFA
 emptyStringDfa ab = DFA {dstart = 0, 
-                         dstates = Map.fromList [(0,True),(1,False)],
+                         dstates = Set.fromList [0,1],
+                         daccept = Set.singleton 0,
                          dtransition = Map.fromList [((0,s),1) | s <- Set.toList ab],
                          dalphabet = ab}
+
+
 
 class Automata a where
   decideString :: a -> String -> Maybe Bool
@@ -69,7 +82,7 @@ instance Automata DFA where
                                          Just q'  -> decideStringFromState dfa cs q'
                                          Nothing  -> decideStringFromState dfa cs q
       | otherwise                    = Nothing
-    decideStringFromState dfa [] q   = Map.lookup q (dstates dfa)
+    decideStringFromState dfa [] q   = Just (Set.member q (daccept dfa))
 
 -- TODO: more tests
 testDecideStringDfa :: Test
@@ -104,21 +117,20 @@ instance Automata NFA where
                           Nothing  -> Set.empty
           in decideStringFromState nfa cs (Set.union qs' eqs')
       | otherwise                    = Nothing
-    decideStringFromState nfa [] qs  = any id <$> accepts where 
-                                       accepts :: Maybe [Bool]
-                                       accepts = mapM (\q -> Map.lookup q $ nstates nfa) (Set.toList qs)
+    decideStringFromState nfa [] qs  = Just $ any accepts (Set.toList qs) where
+                                       accepts q = Set.member q $ naccept nfa
 
 stateBijections :: States -> States -> [Map State State]
-stateBijections s1 s2 = let xs = Map.keys s1 
-                            perms = List.permutations (Map.keys s2) in 
+stateBijections s1 s2 = let xs = Set.toList s1 
+                            perms = List.permutations (Set.toList s2) in 
                             Map.fromList <$> (fmap (\perm -> zip xs perm) perms)
 
 testBijections :: Test
 testBijections = "bijections" ~: 
-  let states1 = Map.fromList [(1, True),(2, False),(3, True)] in
-  let states2 = Map.fromList [(4, True),(5, False),(6, True)] in
+  let states1 = Set.fromList [1,2,3] in
+  let states2 = Set.fromList [4,5,6] in
     TestList[
-        stateBijections (Map.fromList [(0, True)]) (Map.fromList [(0, True)]) 
+        stateBijections (Set.singleton 0) (Set.singleton 0)
           ~?= [Map.fromList [(0,0)]],
         stateBijections states1 states2 ~?= [Map.fromList [(1,4),(2,5),(3,6)],
                                              Map.fromList [(1,5),(2,4),(3,6)],
@@ -170,9 +182,7 @@ testTransitionsSame = "transitions same" ~:
 
 acceptStatesSame :: Map State State -> DFA -> DFA -> Bool
 acceptStatesSame m d1 d2 = all (\(q1,q2) -> equivAccept q1 q2) (Map.toList m)
-  where equivAccept q1 q2 = case (Map.lookup q1 (dstates d1), Map.lookup q2 (dstates d2)) of
-                              (Just b1, Just b2) -> b1 == b2
-                              _ -> False
+  where equivAccept q1 q2 = Set.member q1 (daccept d1) == Set.member q2 (daccept d2)
 
 testAcceptStatesSame :: Test
 testAcceptStatesSame = "accept states same" ~: 
@@ -189,7 +199,7 @@ testAcceptStatesSame = "accept states same" ~:
 instance Eq DFA where
   (==) d1 d2 = 
     dalphabet d1 == dalphabet d2
-    && Map.size (dstates d1) == Map.size (dstates d2)
+    && Set.size (dstates d1) == Set.size (dstates d2)
     && Map.size (dtransition d1) == Map.size (dtransition d2)
     && any isIsomorphicMapping (stateBijections (dstates d1) (dstates d2))
     where isIsomorphicMapping :: Map State State -> Bool
@@ -209,6 +219,15 @@ testEqDFA = "test isomorphic DFA" ~:
         d1 == d2 ~?= False,
         d2 == d1 ~?= False
       ]
+
+-- We implement NFA equality as exact equality
+instance Eq NFA where
+  (==) n1 n2 = 
+    nalphabet n1 == nalphabet n2
+    && nstates n1 == nstates n2
+    && naccept n1 == naccept n2
+    && ntransition n1 == ntransition n2
+    && nstart n1 == nstart n2
 
 main :: IO ()
 main = do
--- Construction.hs
+++ Construction.hs
@@ -9,30 +9,80 @@ import qualified Data.Map as Map
 import Data.Set.Monad (Set)
 import qualified Data.Set.Monad as Set
 
+import Test.HUnit (Test(..), (~:), (~?=), runTestTT, assertBool)  
+
 --TODO: how to define alphabet here 
 
 thompsonConstruction :: RegExp -> DFA
 thompsonConstruction regexp = let nfa = thompsonNfaConstruction regexp
-															in dfaConstruction nfa
+                              in dfaConstruction nfa
 
 thompsonNfaConstruction :: RegExp -> NFA
 thompsonNfaConstruction = undefined
 
-singleCharThompson :: Set Char -> Maybe Char -> NFA --TODO: Change type signature to DFA when DFA converter is implemented 
-singleCharThompson ab char = let singleStates = Map.insert 1 True (Map.singleton 0 False) in 
-                             let singleTransition = Map.insert (0, char) (Set.singleton 1) Map.empty in
-                                 NFA {nstart = 0, nstates = singleStates, ntransition = singleTransition, nalphabet = ab}
+singleCharThompson :: Char -> NFA --TODO: Change type signature to DFA when DFA converter is implemented 
+singleCharThompson char = 
+  let ab = Set.singleton char
+      singleStates = Set.fromList [0,1]
+      singleTransition = Map.fromList [((0, Just char), Set.singleton 1)]
+      singleAccept = Set.singleton 1
+  in NFA {nstart = 0, 
+          nstates = singleStates, 
+          naccept = singleAccept,
+          ntransition = singleTransition, 
+          nalphabet = ab}
 
-unionThompson :: Set Char -> RegExp -> NFA
-unionThompson ab (Alt r1 r2) = let n1 = thompsonNfaConstruction r1 in
-                               let n2 = thompsonNfaConstruction r2 in
-                                      let firstUnion = Map.union (Map.union (Map.mapKeys (+1) (nstates n1)) (Map.mapKeys (+ (Map.size (nstates n1) - 1)) (nstates n2))) (Map.singleton 0 False) in
-                                        let unionStates = Map.union firstUnion (Map.singleton  ((Map.size firstUnion) - 1) True)  in 
-                                        let unionTransition = Map.union (Map.union (Map.mapKeys (\(a,b) -> (a + 1,b)) (ntransition n1)) (Map.mapKeys (\(a,b) -> (a + Map.size (ntransition n1) - 1,b)) (ntransition n2))) (Map.singleton (0,Nothing) (Set.fromList [1, Map.size (nstates n1)])) in 
-                                            let finalTransition = Map.union (Map.union (unionTransition) (Map.singleton ((Map.size unionTransition) - 1, Nothing) (Set.singleton $ Map.size unionTransition))) (Map.singleton (Map.size (nstates n1),Nothing) (Set.singleton $ Map.size unionTransition)) in
-                                                NFA {nstart = 0, nstates = unionStates, ntransition = unionTransition, nalphabet = ab}
+testSingleCharThompson :: Test
+testSingleCharThompson = TestList [
+  singleCharThompson 'a' ~?= NFA {
+    nstart = 0,
+    nstates = Set.fromList [0,1],
+    naccept = Set.singleton 1,
+    ntransition = Map.fromList [((0,Just 'a'), Set.singleton 1)],
+    nalphabet = Set.singleton 'a'
+  }]
 
+unionThompson :: NFA -> NFA -> NFA
+unionThompson n1 n2 = 
+  let ab = Set.union (nalphabet n1) (nalphabet n2)
+      lastStateN1 = Set.size (nstates n1)
+      firstStateN2 = lastStateN1 + 1
+      lastStateN2 = lastStateN1 + Set.size (nstates n2)
+      lastStateUnion = lastStateN2 + 1
+      st0 = Set.union 
+             (fmap (+1) (nstates n1)) 
+             (fmap (+ firstStateN2) (nstates n2))
+      st1 = Set.insert lastStateUnion st0
+      states = Set.insert 0 st1
+      incN1T = fmap (fmap (+1)) $ 
+                 Map.mapKeys (\(a,b) -> (a + 1,b)) (ntransition n1)
+      incN2T = fmap (fmap (+ firstStateN2)) $
+                 Map.mapKeys (\(a,b) -> (a + firstStateN2,b)) (ntransition n2)
+      ut0 = Map.union incN1T incN2T
+      ut1 = Map.insert (0, Nothing) (Set.fromList [1, firstStateN2]) ut0
+      ut2 = Map.insert (lastStateN1, Nothing) (Set.singleton lastStateUnion) ut1
+      transitions = Map.insert (lastStateN2, Nothing) (Set.singleton lastStateUnion) ut2
+      accepts = Set.singleton lastStateUnion
+  in NFA {nstart = 0, 
+          nstates = states,
+          naccept = accepts,
+          ntransition = transitions, 
+          nalphabet = ab}
 
+testUnionThompson :: Test
+testUnionThompson = TestList [
+  unionThompson (singleCharThompson 'a') (singleCharThompson 'b') ~?= 
+    NFA {
+      nstart = 0,
+      nstates = Set.fromList [0,1,2,3,4,5],
+      naccept = Set.singleton 5,
+      ntransition = Map.fromList [((0,Nothing), Set.fromList [1,3]),
+                                  ((1, Just 'a'), (Set.singleton 2)),
+                                  ((3, Just 'b'), (Set.singleton 4)),
+                                  ((2,Nothing), (Set.singleton 5)),
+                                  ((4,Nothing), (Set.singleton 5))],
+      nalphabet = Set.fromList "ab"
+    }]
 
 concatThompson :: Set Char -> RegExp -> NFA
 concatThompson ab (Seq r1 r2) = undefined 
@@ -46,3 +96,7 @@ brzozowskiConstruction = undefined
 dfaConstruction :: NFA -> DFA 
 dfaConstruction = undefined 
 
+main :: IO ()
+main = do
+    runTestTT $ TestList [ testSingleCharThompson, testUnionThompson]
+    return ()
