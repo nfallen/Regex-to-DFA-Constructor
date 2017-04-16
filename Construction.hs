3f9284fc018bc3b97f39246c51b254b37f9cb88a
Regina <rburd@seas.upenn.edu>
2017-04-16 12:51:17 -0400

    Implement all thompsonNfaConstruction pattern matching with exception of Char c

--- Construction.hs
+++ Construction.hs
@@ -11,16 +11,24 @@ import qualified Data.Map as Map
 import Data.Set.Monad (Set)
 import qualified Data.Set.Monad as Set
 
+import Text.Parsec as P hiding (Empty)
+
 import Test.HUnit (Test(..), (~:), (~?=), runTestTT, assertBool)  
 
---TODO: how to define alphabet here 
-
 thompsonConstruction :: RegExp -> DFA
+thompsonConstruction Empty = emptyStringDfa (Set.empty)
+thompsonConstruction Void = emptySetDfa (Set.empty)
+thompsonConstruction (Var s) = case (P.parse regexParser "" s) of 
+									Left err -> emptySetDfa (Set.empty) --creates the void dfa upon invalid string input
+									Right regexp -> thompsonConstruction regexp 
 thompsonConstruction regexp = let nfa = thompsonNfaConstruction regexp
                               in dfaConstruction nfa
 
 thompsonNfaConstruction :: RegExp -> NFA
-thompsonNfaConstruction = undefined
+thompsonNfaConstruction (Char c) = undefined 
+thompsonNfaConstruction (Alt r1 r2) = unionNfa (thompsonNfaConstruction r1) (thompsonNfaConstruction r2) 
+thompsonNfaConstruction (Seq r1 r2) = concatNfa (thompsonNfaConstruction r1) (thompsonNfaConstruction r2) 
+thompsonNfaConstruction (Star r) = kleeneNfa (thompsonNfaConstruction r)
 
 brzozowskiConstruction :: RegExp -> DFA
 brzozowskiConstruction = undefined
